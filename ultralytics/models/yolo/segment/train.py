410173bfc934f102c73d9a7aa7643151e3f98230
stanley <stanleywu@chase.com.tw>
2024-10-01 14:02:10 +0800

    modify init model

--- ultralytics/models/yolo/segment/train.py
+++ ultralytics/models/yolo/segment/train.py
@@ -31,7 +31,7 @@ class SegmentationTrainer(yolo.detect.DetectionTrainer):
 
     def get_model(self, cfg=None, weights=None, verbose=True):
         """Return SegmentationModel initialized with specified config and weights."""
-        model = SegmentationModel(cfg, ch=3, nc=self.data["nc"], verbose=verbose and RANK == -1)
+        model = SegmentationModel(cfg, ch=cfg["ch"], nc=self.data["nc"], verbose=verbose and RANK == -1)
         if weights:
             model.load(weights)
 
